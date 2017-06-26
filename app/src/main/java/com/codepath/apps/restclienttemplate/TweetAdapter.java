ca299b833ef67e05f0d7a6407fcaf56dd350b8b2
Riya Uppal <ruppal@fb.com>
2017-06-26 15:40:27 -0700

    tweets appear. include pictre, username, screen name, timestamp, body, and line separating tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -2,15 +2,22 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.apps.restclienttemplate.models.Tweet;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Locale;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 /**
  * Created by ruppal on 6/26/17.
@@ -19,6 +26,7 @@ import java.util.List;
 public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
 
     private List<Tweet> mTweets;
+    Context context;
     //pass the tweets array into the constructor
 
     public TweetAdapter(List<Tweet> tweets){
@@ -30,7 +38,7 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        Context context = parent.getContext();
+        context = parent.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View tweetView=inflater.inflate(R.layout.item_tweet, parent, false);
         ViewHolder viewHolder = new ViewHolder(tweetView);
@@ -45,7 +53,17 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
         //populate the views according to the data from this specific tweet
         holder.tvUsername.setText(currentTweet.user.name);
         holder.tvBody.setText(currentTweet.body);
-        //TODO-load the image using glide
+        //TODO-add a placeholder picture while image is loading
+        int radius = 8; // corner radius, higher value = more rounded
+        int margin = 3; // crop margin, set to 0 for corners with no crop
+        Glide.with(context)
+                .load(currentTweet .user.profileImageUrl)
+                .bitmapTransform(new RoundedCornersTransformation(context, radius, margin))
+                .into(holder.ivProfileImage);
+        holder.tvScreenName.setText(String.format("@%s", currentTweet.user.screenName));
+        String relativeTimeAgo=getRelativeTimeAgo(currentTweet.createdAt);
+        holder.tvRelativeTimestamp.setText(relativeTimeAgo);
+
     }
 
     @Override
@@ -58,6 +76,8 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
         public ImageView ivProfileImage;
         public TextView tvBody;
         public TextView tvUsername;
+        public TextView tvScreenName;
+        public TextView tvRelativeTimestamp;
 
         public ViewHolder(View itemView){
             super(itemView);
@@ -65,7 +85,27 @@ public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder>{
             ivProfileImage = (ImageView) itemView.findViewById(R.id. ivProfileImage);
             tvBody = (TextView) itemView.findViewById(R.id.tvBody);
             tvUsername = (TextView) itemView.findViewById(R.id.tvUsername);
+            tvScreenName = (TextView) itemView.findViewById(R.id.tvScreenName);
+            tvRelativeTimestamp = (TextView) itemView.findViewById(R.id.tvRelativeTimestamp);
 
         }
+    }
+
+    // getRelativeTimeAgo("Mon Apr 01 21:16:23 +0000 2014");
+    public String getRelativeTimeAgo(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
     }
 }
